5e1707ddf3315cd329f611d993e7077f318278e7
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-10 23:02:36 +0900

    バリデーション、新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, last_name, :first_name_kana, last_name_kana, birthday ])
-  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]   #追加部分
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']   # 追加部分
+    end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,
+                                        keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
     end
   end
 end
