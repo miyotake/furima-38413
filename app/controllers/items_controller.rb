433fa0b27cefd54d84fd3a181f1207a25e338241
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-12 00:53:49 +0900

    新規登録、ログイン、ログアウト、エラーメッセージ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
   def index
-  end
-end
-
-
-
-
-
-
-private
-
-
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_08_124844) do
+ActiveRecord::Schema.define(version: 2023_01_11_135819) do
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
