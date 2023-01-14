34874a95727c22bb45b1b58be896541b0ae07fa1
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-14 17:22:13 +0900

    修正
    不要記述の削除
    マイグレーション変更に伴う
    rollback&migrate

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2023_01_11_135819) do
     t.string "last_name", default: "", null: false
     t.string "first_name_kana", default: "", null: false
     t.string "last_name_kana", default: "", null: false
-    t.datetime "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
