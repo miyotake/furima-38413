6827f3f62456a3ee8a969ddfdea2183fa4c18cd0
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-18 16:09:04 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :delivery_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :shipping_id, numericality: { other_than: 0　, message: "can't be blank" }
+  validates :shipping_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :image, presence: true
   validates :price, numericality: { less_than_or_equal_to: 9_999_999, greater_than_or_equal_to: 300, only_integer: true }
 
