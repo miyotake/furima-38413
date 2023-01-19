8df9527a810a9b8aeadbcbf85093cb7be58a36f8
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-19 18:40:09 +0900

    商品詳細機能

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,9 +1,9 @@
 class Shipping < ActiveHash::Base
 self.data = [
-  { id: 0, shipping: '---' },
-  { id: 1, shipping: '1~2日で発送' },
-  { id: 2, shipping: '2~3日で発送' },
-  { id: 3, shipping: '4~7日で発送' }
+  { id: 0, name: '---' },
+  { id: 1, name: '1~2日で発送' },
+  { id: 2, name: '2~3日で発送' },
+  { id: 3, name: '4~7日で発送' }
 ]
 include ActiveHash::Associations
 has_many :items
