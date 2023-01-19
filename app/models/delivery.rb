112e71fa848635c853f3b302b8a9e611337fe9e5
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-19 19:47:32 +0900

    Revert "Revert "Revert "Revert "Revert "商品詳細機能"""""
    This reverts commit 8444fb1e66518dc6a64e9b170143829e1083992a.

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,9 +2,9 @@
   class Delivery < ActiveHash::Base 
 
     self.data = [
-         { id: 0, name: '---' },
-         { id: 1, name: '着払い(購入者負担)' },
-         { id: 2, name: '送料込み(出品者負担)' },
+         { id: 0, delivery: '---' },
+         { id: 1, delivery: '着払い(購入者負担)' },
+         { id: 2, delivery: '送料込み(出品者負担)' },
          ]
        include ActiveHash::Associations
        has_many :items
