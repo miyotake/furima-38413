7723234fb30ba034a5993ce4cbdf5c92d7d8a879
miyotake <113184420+miyotake@users.noreply.github.com>
2023-01-19 18:40:59 +0900

    Revert "商品一覧表示機能"
    This reverts commit a3a92a13a13a8e92c1beea75b7550c1c5a83e9a0.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :set_item, only: [:edit, :update, :show, :destroy]
+
   def index
-    @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -17,7 +16,6 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
     private
     def item_params
       params.require(:item).permit(:name, :price, :description, :condition_id, :delivery_id, :shipping_id, :prefecture_id,
@@ -31,9 +29,5 @@ class ItemsController < ApplicationController
         redirect_to root_path
       end
 end
-def set_item
-  @item = Item.find(params[:id])
-end
-
 end
 
